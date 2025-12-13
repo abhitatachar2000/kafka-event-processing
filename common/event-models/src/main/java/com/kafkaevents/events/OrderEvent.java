cf6c26e3de7efec7f00dc95f773016f8fdd2f1db
abhitatachar2000 <abhishektatachar@gmail.com>
2025-12-13 16:25:25 +0530

    basic project structure

--- common/event-models/src/main/java/com/kafkaevents/events/OrderEvent.java
+++ common/event-models/src/main/java/com/kafkaevents/events/OrderEvent.java
@@ -1,7 +1,7 @@
 package com.kafkaevents.events;
 import java.time.Instant;
 
-public class OrdersEvent {
+public class OrderEvent {
     private String orderId;
     private String productId;
     private int quantity;
